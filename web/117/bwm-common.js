73b782caec327fd773f5d3311eaa0baf358db710
tomato.taiwan <tomato.taiwan@4102177b-c944-0410-a74c-b79a4a1f0c97>
2008-03-02 14:03:32 +0000

    Update for bwm-daily.asp

--- web/117/bwm-common.js
+++ web/117/bwm-common.js
@@ -24,9 +24,9 @@ var avgMode = 0;
 var avgLast = -1;
 var colorX = 0;
 var colors = [	
-	['Green &amp; Blue', '#118811', '#6495ed'], ['Blue &amp; Orange', '#003EBA', '#FF9000'],
-	['Blue &amp; Red', '#003EDD', '#CC4040'], ['Blue', '#22f', '#225'], ['Gray', '#000', '#999'],
-	['Red &amp; Black', '#d00', '#000']];
+	['綠色 &amp; 藍色', '#118811', '#6495ed'], ['藍色 &amp; Orange', '#003EBA', '#FF9000'],
+	['藍色 &amp; 紅色', '#003EDD', '#CC4040'], ['藍色', '#22f', '#225'], ['灰色', '#000', '#999'],
+	['紅色 &amp; 黑色', '#d00', '#000']];
 
 function xpsb(byt)
 {
