2f76f616860be93ce0f1e69791ed0c4ca1387007
ddwrt.taiwan <ddwrt.taiwan@4102177b-c944-0410-a74c-b79a4a1f0c97>
2008-05-10 00:09:27 +0000

    recheck src.1.3

--- trunk/source/119_qos_conanxu/src/router/rc/wan.c
+++ trunk/source/119_qos_conanxu/src/router/rc/wan.c
@@ -741,7 +741,8 @@ void start_wan_done(char *wan_ifname)
 	start_firewall();
 	start_qos();
 	new_qoslimit_start();
-
+	new_arpbind_start();
+	
 	stop_igmp_proxy();
 	start_igmp_proxy();
 	
@@ -788,6 +789,7 @@ void stop_wan(void)
 	
 	_dprintf("%s: begin\n", __FUNCTION__);
 
+	new_arpbind_stop();
 	new_qoslimit_stop();
 	stop_qos();
 	stop_firewall();
